ab229adae9a4a5e3dfeca546fc499ef65548a70d
Shuntoria Reid <shuntoriareid240@gmail.com>
2024-02-28 04:14:16 +0000

    update

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,8 +1,9 @@
 code --install-extension redhat.java@1.22.1
 code --install-extension vscjava.vscode-java-debug
-Start-Sleep -s 40
+sleep 40
 code --install-extension revaturePro.revature-labs
-for ($i=0; $i -lt 3; $i++) {
-    code --uninstall-extension vscjava.vscode-java-test
-    Start-Sleep -s 15
-}
+for i in {1..3}
+do
+   code --uninstall-extension vscjava.vscode-java-test
+   sleep 15
+done
